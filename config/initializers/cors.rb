2eb19d32440f9b96e9c15a89499f7747d679728d
LancharroDav <49806971+LancharroDav@users.noreply.github.com>
2021-03-13 10:34:33 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:3000'
     elsif Rails.env.production?
-      origins 'https://devreturns-front.vercel.app'
+      # origins '[here production's front-end URL]'
     end
     resource '*', 
       headers: :any, methods: [:get, :post, :patch, :put, :delete, :options],
